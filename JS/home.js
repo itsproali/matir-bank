26c6b28212b534880c198466741efa881634a281
Mohammad Ali <itsproali@gmail.com>
2022-03-10 18:35:03 +0600

    Local Storage Added

--- JS/home.js
+++ JS/home.js
@@ -1,10 +1,15 @@
 let currentBalance = document.getElementById('balance');
-// Common
+// Get Input Field Values
 function getInput(inputID) {
     let inputField = document.getElementById(inputID).value;
-    const inputAmount = Number(inputField);
-    document.getElementById(inputID).value = '';
-    return inputAmount;
+    if (inputField === '') {
+        alert('Please Enter a valid Number');
+        return;
+    } else {
+        const inputAmount = Number(inputField);
+        document.getElementById(inputID).value = '';
+        return inputAmount;
+    }
 };
 // Get Total Deposit and Withdraw
 function getTotal(totalID, amount) {
@@ -17,7 +22,7 @@ function updateBalance(inputAmount, isAdd) {
     if (isAdd === true) {
         currentBalance.innerText = Number(currentBalance.innerText) + inputAmount;
     } else {
-        latestBalance = Number(currentBalance.innerText) - inputAmount;
+        const latestBalance = Number(currentBalance.innerText) - inputAmount;
         return latestBalance;
     }
 };
@@ -26,27 +31,66 @@ function updateBalance(inputAmount, isAdd) {
 document.getElementById('deposit-button').addEventListener('click', function () {
     let depositAmount = getInput('deposit-input');
     // Error Handle
-    if (isNaN(depositAmount) || depositAmount < 0) {
+    if (depositAmount === undefined) {
+        return;
+    } else if (isNaN(depositAmount) || depositAmount < 0) {
         alert('Please enter a valid number');
         return;
     }
     getTotal('deposit', depositAmount);
     updateBalance(depositAmount, true);
+    addToStorage('Deposit', 'deposit');
+    addBalanceToStorage();
 });
 
 // Handle Withdraw
 document.getElementById('withdraw-button').addEventListener('click', function () {
     let withdrawAmount = getInput('withdraw-input')
     // Error Handle
-    if (isNaN(withdrawAmount) || withdrawAmount < 0) {
+    if (withdrawAmount === undefined) {
+        return;
+    } else if (isNaN(withdrawAmount) || withdrawAmount < 0) {
         alert('Please enter a valid number');
         return;
     }
     const updatedBalance = updateBalance(withdrawAmount, false);
     if (updatedBalance < 0) {
-        alert("!!!You don't have enough money. Please try to input lower amount");
+        alert("!!! You don't have enough money to withdraw !!!");
         return;
     }
     currentBalance.innerText = updatedBalance;
     getTotal('withdraw', withdrawAmount);
+    addToStorage('Withdraw', 'withdraw');
+    addBalanceToStorage();
 });
+
+// Add Deposit or Withdraw total to Local Storage
+const addToStorage = (name, id) => {
+    const displayedMoney = document.getElementById(id).innerText;
+    localStorage.setItem(name, displayedMoney);
+}
+// Add Current Balance to Local Storage
+const addBalanceToStorage = () => {
+    localStorage.setItem('Balance', currentBalance.innerText);
+}
+// Display Stored Deposit/withdraw total to the ui
+const displayStoredValue = (name, id) => {
+    const storedMoney = document.getElementById(id);
+    const getMoney = localStorage.getItem(name);
+    storedMoney.innerText = getMoney;
+}
+// Display Stored Balance to ui
+const displayStoredBalance = () => {
+    const storedBalance = localStorage.getItem('Balance');
+    currentBalance.innerText = storedBalance;
+}
+
+// Display Previous Stored Data
+displayStoredValue('Deposit', 'deposit');
+displayStoredValue('Withdraw', 'withdraw');
+
+if (localStorage.getItem('Balance')) {
+    displayStoredBalance();
+} else {
+    console.log('Hello World!!');
+}
